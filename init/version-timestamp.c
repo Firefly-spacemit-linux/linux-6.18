3573cbccc3caaf408acf4f9b1b45f3a545bc8000
lvsx <lvsx@t-chip.com.cn>
2026-04-13 13:56:14 +0800

    ns: add LINUX_BRANCH,LINUX_COMMIT,LINUX_COMPILE_TIME,XML_NAME to version-timestamp

--- init/version-timestamp.c
+++ init/version-timestamp.c
@@ -28,4 +28,4 @@ struct uts_namespace init_uts_ns = {
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
 	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
-	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
+	LINUX_COMPILE_HOST ") (" LINUX_BRANCH ": " LINUX_COMMIT "/" LINUX_COMPILE_TIME ") (" XML_NAME ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
